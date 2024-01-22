17eee3da049fd719cab09a83c4e2953492813d1a
Melanchenko Andriy <ipz232_mad@student.ztu.edu.ua>
2024-01-22 22:31:38 +0200

    created new imitated class - Monitor on base class Product. There was added fields: UpdateFrequency, DisplayDiagonal, MatrixType, Color. Was added constructor by default and constructor with all parameters. Method Display was overrided and return all information about monitor.

---
+++ Sana07/Monitor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sana07
+{
+    public class Monitor : Product
+    {
+        protected int UpdateFrequency { get; set; }
+        protected double DisplayDiagonal { get; set; }
+        protected string MatrixType { get; set; }
+        protected string Color { get; set; }
+        public Monitor(string name, double price, int count, int updateFrequency, double displayDiagonal, string matrixType, string color)
+            : base(name, price, count)
+        {
+            UpdateFrequency = updateFrequency;
+            DisplayDiagonal = displayDiagonal;
+            MatrixType = matrixType;
+            Color = color;
+        }
+        public Monitor()
+        {
+            UpdateFrequency = 0;
+            DisplayDiagonal = 0;
+            MatrixType = "NM";
+            Color = "NM";
+        }
+        public override string Display()
+        {
+            return $"{Name} {Price} {Count} {UpdateFrequency} {DisplayDiagonal} {MatrixType} {Color}";
+        }
+    }
+}
