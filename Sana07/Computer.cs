96e621ad3ce67f23320579b340fa2d21f1ab5fc2
Melanchenko Andriy <ipz232_mad@student.ztu.edu.ua>
2024-01-22 22:52:49 +0200

    created new imitated class - Computer on base class Product. There was added fields: PowerOfPowerSupply, RAM_Memory, VideoMemory, SSD_Memory. Was added constructor by default and constructor with all parameters. Method Display was overrided and return all information about Computer

---
+++ Sana07/Computer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sana07
+{
+    public class Computer:Product
+    {
+        protected int PowerOfPowerSupply {  get; set; }
+        protected int RAM_Memory { get; set; }
+        protected int VideoMemory { get; set; }
+        protected int SSD_Memory { get; set; }
+        public Computer(string name, double price, int count, int powerOfPowerSupply, int rAM_Memory, int videoMemory, int sSD_Memory)
+            : base(name, price, count)
+        {
+            PowerOfPowerSupply = powerOfPowerSupply; 
+            RAM_Memory = rAM_Memory;
+            VideoMemory = videoMemory;
+            SSD_Memory = sSD_Memory;
+        }
+        public Computer()
+        {
+            PowerOfPowerSupply = 0;
+            RAM_Memory = 0;
+            VideoMemory = 0;
+            SSD_Memory = 0;
+        }
+        public override string Display()
+        {
+            return $"{Name} {Price} {Count} {PowerOfPowerSupply} {RAM_Memory} {VideoMemory} {SSD_Memory}";
+        }
+    }
+}
