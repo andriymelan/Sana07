f16f49361b6541dd91a36bf750fca20010ab8561
Melanchenko Andriy <ipz232_mad@student.ztu.edu.ua>
2024-01-22 22:40:48 +0200

    created new imitated class - Smartphone on base class Product. There was added fields: DisplayDiagonal, RAM, BuiltMemory, BatteryCapacity. Was added constructor by default and constructor with all parameters. Method Display was overrided and return all information about Smartphone

---
+++ Sana07/Smartphone.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sana07
+{
+    public class Smartphone : Product
+    {
+        protected double DisplayDiagonal { get; set; }
+        protected int RAM { get; set; }
+        protected int BuiltMemory { get; set; }
+        protected int BatteryCapacity { get; set; }
+        public Smartphone(string name, double price, int count, double displayDiagonal, int rAM, int builtMemory, int batteryCapacity)
+            : base(name, price, count)
+        {
+            DisplayDiagonal = displayDiagonal;
+            RAM = rAM;
+            BuiltMemory = builtMemory;
+            BatteryCapacity = batteryCapacity;
+        }
+        public Smartphone()
+        {
+            DisplayDiagonal = 0;
+            RAM = 0;
+            BuiltMemory = 0;
+            BatteryCapacity = 0;
+        }
+        public override string Display()
+        {
+            return $"{Name} {Price} {Count} {DisplayDiagonal} {RAM} {BuiltMemory} {BatteryCapacity}";
+        }
+    }
+}
